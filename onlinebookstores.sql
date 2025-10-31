ab730e3196764d228230c0aad3c290c7644d2921
shefeenasiyad <shefeenaraja@gmail.com>
2025-10-31 12:15:12 +0530

    Add files via upload

--- onlinebookstores.sql
+++ onlinebookstores.sql
@@ -1,111 +1,58 @@
--- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
---
--- Host: localhost    Database: onlinebookstores
--- ------------------------------------------------------
--- Server version	8.0.28
+USE onlinebookstores;
+CREATE TABLE books(
+book_id VARCHAR(5) NOT NULL PRIMARY KEY,
+title VARCHAR(50) NOT NULL,
+author_id VARCHAR(5) NOT NULL ,
+price FLOAT NOT NULL,
+publication_year INT NOT NULL,
+FOREIGN KEY(author_id)REFERENCES authors(author_id)
+);
+USE onlinebookstores;
+CREATE TABLE authors(
+author_id VARCHAR(5) NOT NULL PRIMARY KEY,
+author_name VARCHAR(50),
+country VARCHAR(50)
+);
+USE onlinebookstores;
+CREATE TABLE orders(
+order_id VARCHAR(5) NOT NULL PRIMARY KEY,
+book_id  VARCHAR(5) NOT NULL,
+customer_name VARCHAR(50) NOT NULL,
+order_date DATE NOT NULL,
+FOREIGN KEY(book_id) REFERENCES books(book_id) 
+);
+INSERT INTO books(book_id, title,author_id, price, publication_year)
+VALUES
+('b1','Harrypotter','a1',  25, 2025),
+('b2','Balyakala sakhi','a2', 35, 2024),
+('b3', 'Indica','a3',32, 2024),
+('b4','Cricket my style','a4', 45, 2020),
+('b5','Arthasastra','a5',  45, 2021);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+INSERT INTO authors( author_id,author_name, country)
+VALUES
+('a1','JK Rowling' , 'England'),
+('a2', 'Vaikom Muhammed Basheer','India'),
+('a3', 'Megesthenes', 'Greece'),
+('a4', 'Kapil Dev', 'India'),
+('a5', 'Chanakya', 'India');
 
---
--- Table structure for table `authors`
---
-
-DROP TABLE IF EXISTS `authors`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `authors` (
-  `author_id` varchar(5) NOT NULL,
-  `author_name` varchar(50) DEFAULT NULL,
-  `country` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`author_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `authors`
---
-
-LOCK TABLES `authors` WRITE;
-/*!40000 ALTER TABLE `authors` DISABLE KEYS */;
-INSERT INTO `authors` VALUES ('a1','JK Rowling','England'),('a2','Vaikom Muhammed Basheer','India'),('a3','Megesthenes','Greece'),('a4','Kapil Dev','India'),('a5','Chanakya','India');
-/*!40000 ALTER TABLE `authors` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `books`
---
-
-DROP TABLE IF EXISTS `books`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `books` (
-  `book_id` varchar(5) NOT NULL,
-  `title` varchar(50) NOT NULL,
-  `author_id` varchar(5) NOT NULL,
-  `price` float NOT NULL,
-  `publication_year` int NOT NULL,
-  `generation` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`book_id`),
-  KEY `author_id` (`author_id`),
-  CONSTRAINT `books_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `authors` (`author_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `books`
---
-
-LOCK TABLES `books` WRITE;
-/*!40000 ALTER TABLE `books` DISABLE KEYS */;
-INSERT INTO `books` VALUES ('b1','Harrypotter','a1',25,2025,NULL),('b2','Balyakala sakhi','a2',35,2024,NULL),('b3','Indica','a3',32,2024,NULL),('b4','Cricket my style','a4',45,2020,NULL),('b5','Arthasastra','a5',45,2021,NULL);
-/*!40000 ALTER TABLE `books` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `orders`
---
-
-DROP TABLE IF EXISTS `orders`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `orders` (
-  `order_id` varchar(5) NOT NULL,
-  `book_id` varchar(5) NOT NULL,
-  `customer_name` varchar(50) NOT NULL,
-  `order_date` date NOT NULL,
-  `quantity` int DEFAULT NULL,
-  PRIMARY KEY (`order_id`),
-  KEY `book_id` (`book_id`),
-  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `orders`
---
-
-LOCK TABLES `orders` WRITE;
-/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
-INSERT INTO `orders` VALUES ('o1','b1','Sreeja','2021-10-09',NULL),('o2','b2','remya','2020-11-07',NULL),('o3','b3','Mohan','2020-10-08',NULL),('o4','b4','Malu','2000-05-04',NULL),('o5','b5','vinu','2001-06-11',NULL);
-/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2025-10-30 13:33:29
+INSERT  INTO orders( order_id,book_id,customer_name,order_date)
+VALUES
+('o1','b1','Sreeja','2021,10,9'),
+('o2','b2', 'remya','2020,11,7'),
+('o3', 'b3','Mohan','2020,10,8'),
+('o4','b4', 'Malu','2000,5,4'),
+('o5','b5', 'vinu','2001,6,11');
+SELECT * FROM authors;
+ALTER TABLE books ADD generation VARCHAR(50);
+SELECT * FROM books;
+ALTER TABLE orders ADD quantity INT;
+SELECT * FROM orders;
+SELECT  books.title,
+authors.author_name FROM books
+INNER JOIN authors ON books.author_id=authors.author_id;
+SELECT orders.order_id,
+books.title,
+orders.customer_name,
+orders.order_date FROM orders INNER JOIN books ON orders.book_id=books.book_id;
